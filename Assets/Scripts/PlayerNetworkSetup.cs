2e4cdd1616cc1b20000867433d95065e48ff3cee
roshando <allan.sequeira@gmail.com>
2015-07-18 11:23:23 -0500

    - disabled Player's (FPSController) Character Controller and First Person Controller (will enable in PlayerNetworkSetup script) - disabled FirstPersonCharacter camera and audiolistener (will enable in PlayerNetworkSetup script) - created PlayerNetworkSetup script and added it to Player game object - configured the PlayerNetworkSetup script with FirstPersonCharacter camera and audiolistener

---
+++ Assets/Scripts/PlayerNetworkSetup.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class PlayerNetworkSetup : NetworkBehaviour {
+
+	/// <summary>
+	/// The FPS character camera.
+	/// </summary>
+	[SerializeField]Camera FPSCharacterCamera;
+
+	/// <summary>
+	/// The FPS character audio listener.
+	/// </summary>
+	[SerializeField]AudioListener FPSAudioListener;
+
+	// Use this for initialization
+	void Start () {
+
+		if (isLocalPlayer) {
+
+			// deactivate the Scene camera
+			GameObject.Find("Scene Camera").SetActive(false);
+
+			// enable the Character Controller component and first person controller component script (which handles the movement of the FPS)
+			GetComponent<CharacterController>().enabled = true;
+			GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().enabled = true;
+
+			// enable the FPS camera and audiolistener
+			FPSCharacterCamera.enabled = true;
+			FPSAudioListener.enabled = true;
+
+		}
+	
+	}
+
+}
