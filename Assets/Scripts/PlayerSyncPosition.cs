90c34b4b4b3953801663a7976424d0262405c8fd
roshando <allan.sequeira@gmail.com>
2015-07-20 17:57:44 -0500

    - fleshed out PlayerSyncPosition.cs to address syncing movement between different clients - disabled the NetworkTransform component script for player (FPSController) since syncing is handled by the PlayerSyncPosition script - added PlayerSyncPosition script to player and configured the myTransform variable with player instance - applied changes to player prefab

--- Assets/Scripts/PlayerSyncPosition.cs
+++ Assets/Scripts/PlayerSyncPosition.cs
@@ -2,39 +2,65 @@
 using System.Collections;
 using UnityEngine.Networking;
 
-public class PlayerSyncScript : NetworkBehaviour {
+/// <summary>
+/// Sync the players position via this script rather than the Network Transform 
+/// component (Script) that is attached to the Player
+/// </summary>
+public class PlayerSyncPosition : NetworkBehaviour
+{
 
+	/// <summary>
+	/// Server automatically transmit the value to all clients when it changes.
+	/// </summary>
 	[SyncVar]
 	private Vector3 syncPosition;
-	[SerializeField]Transform myTransform;
-	[SerializeField]float lerpRate = 15;
+
+	[SerializeField]
+	Transform myTransform;
+
+	[SerializeField]
+	float lerpRate = 15;
 
 	// Update is called once per frame
-	void FixedUpdate () {
-	
+	void FixedUpdate ()
+	{
+		TransmitPosition ();
+		LerpPosition ();
 	}
 
 
 	/// <summary>
 	/// Lerps the position of the player characters that don't belong to us.
-	/// In out game instance, if there are other players, we want their positions to lerp or their motion to smooth out because position
+	/// In our game instance, if there are other players, we want their positions to lerp or their motion to smooth out because position
 	/// data is going to be received in increments and then players are moved to those new positions that are coming
 	/// i.e. smooth out the movement of the players. This won't run on our game instance.
 	/// </summary>
-	void LerpPosition() {
+	void LerpPosition ()
+	{
 		if (!isLocalPlayer) {
-			myTransform.position = Vector3.Lerp(myTransform.position, syncPosition, Time.deltaTime * lerpRate);
+			myTransform.position = Vector3.Lerp (myTransform.position, syncPosition, Time.deltaTime * lerpRate);
 		}
 	}
 
 	// Since this is server authoritative system, we need to tell the server about this player's position and 
 	// then we want the server to tell all the clients (game instances) where this player's new position is and then
-	// those clients wiil use the LerpPosition function to move those player characters to their positions.
+	// those clients will use the LerpPosition function to move those player characters to their positions.
+	// 
 	// To do this we write a command that is sent to the server i.e. the client is telling the server to run this command
-	// The command must have "Cmd" as the prefix. The client will provide the parameter.
+	// The command must have "Cmd" as the prefix. The client will provide the parameter(s).
 	// This code will only run on the server, but will be called by our game instance client
 	[Command]
-	void CmdProvidePositionToServer(Vector3 pos) {
+	void CmdProvidePositionToServer (Vector3 pos)
+	{
 		syncPosition = pos;
 	}
+
+	[ClientCallback]
+	void TransmitPosition ()
+	{
+		if (isLocalPlayer) {
+			CmdProvidePositionToServer (myTransform.position);
+		}
+	}
 }
+
